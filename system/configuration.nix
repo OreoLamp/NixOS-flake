aaa17ffd77899f1cb07eb5e19216393eca177746
Eero Lampela <eero.lampela@gmail.com>
2023-11-09 13:26:39 +0200

    An attempt at proper vscode config

--- system/configuration.nix
+++ system/configuration.nix
@@ -4,6 +4,7 @@
     lib,
     inputs,
     outputs,
+    nix-vscode-extensions,
     ... 
 }:
 {
@@ -181,9 +182,12 @@
             gnupg # For some reason have to manually specify this???
             libsecret # Required for vscode gpg integration
             atuin # Best shell history
-            vscode-fhs
+            vscode # vscode (duh)
         ]);
     };
+
+    # Makes electron stuff work better???
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     # Makes home-manager load stuff
     home-manager = {
@@ -856,6 +860,8 @@
         enable = true;
         settings = {
             style = "compact";
+	        filter_mode_shell_up_key_binding = "directory";
+	        show_preview = true;
         };
     };
 
@@ -977,6 +983,108 @@
     hm.programs.gpg.homedir = "${config.hm.xdg.dataHome}/gnupg";
     hm.services.gpg-agent.enable = true;
     hm.services.gpg-agent.pinentryFlavor = "gnome3";
+
+    # vscode config
+    hm.programs.vscode = {
+        enable = true;
+        extensions = with nix-vscode-extensions.vscode-marketplace-release; [
+            13xforever.language-x86-64-assembly
+            alefragnani.bookmarks
+            alefragnani.project-manager
+            antiantisepticeye.vscode-color-picker
+            atomiks.moonlight
+            charliermarsh.ruff
+            christian-kohler.path-intellisense
+            cschlosser.doxdocgen
+            davidanson.vscode-markdownlint
+            dbaeumer.vscode-eslint
+            donjayamanne.python-environment-manager
+            eamodio.gitlens
+            ecmel.vscode-html-css
+            enkia.tokyo-night
+            equinusocio.vsc-material-theme
+            equinusocio.vsc-material-theme-icons
+            esbenp.prettier-vscode
+            formulahendry.code-runner
+            fortran-lang.linter-gfortran
+            github.remotehub
+            github.vscode-pull-request-github
+            golang.go
+            grapecity.gc-excelviewer
+            haskell.haskell
+            ibm.output-colorizer
+            jeff-hykin.better-cpp-syntax
+            jnoortheen.nix-ide
+            julialang.language-julia
+            justusadam.language-haskell
+            llvm-vs-code-extensions.vscode-clangd
+            mads-hartmann.bash-ide-vscode
+            mathworks.language-matlab
+            mechatroner.rainbow-csv
+            mhutchie.git-graph
+            miguelsolorio.fluent-icons
+            mikestead.dotenv
+            mkxml.vscode-filesize
+            ms-azuretools.vscode-docker
+            ms-python.autopep8
+            ms-python.black-formatter
+            ms-python.isort
+            ms-python.pylint
+            ms-python.python
+            ms-python.vscode-pylance
+            ms-toolsai.jupyter
+            ms-toolsai.jupyter-keymap
+            ms-toolsai.jupyter-renderers
+            ms-toolsai.vscode-jupyter-cell-tags
+            ms-toolsai.vscode-jupyter-slideshow
+            ms-vscode.azure-repos
+            ms-vscode.cmake-tools
+            ms-vscode.cpptools
+            ms-vscode.cpptools-extension-pack
+            ms-vscode.cpptools-themes
+            ms-vscode.hexeditor
+            ms-vscode.makefile-tools
+            ms-vscode.remote-explorer
+            ms-vscode-remote.remote-containers
+            ms-vscode-remote.remote-ssh
+            ms-vscode-remote.remote-ssh-edit
+            ms-vscode-remote.remote-wsl
+            ms-vscode.remote-repositories
+            ms-vscode.remote-server
+            ms-vscode-remote.vscode-remote-extensionpack
+            ms-vscode.vscode-github-issue-notebooks
+            ms-vscode.vscode-selfhost-test-provider
+            ms-vscode.vscode-serial-monitor
+            njpwerner.autodocstring
+            pkief.material-icon-theme
+            pkief.material-product-icons
+            platformio.platformio-ide
+            rdebugger.r-debugger
+            redhat.java
+            redhat.vscode-xml
+            redhat.vscode-yaml
+            reditorsupport.r
+            rust-lang.rust-analyzer
+            ryu1kn.partial-diff
+            scala-lang.scala
+            scalameta.metals
+            sdras.night-owl
+            shopify.ruby-lsp
+            sonarsource.sonarlint-vscode
+            sumneko.lua
+            tamasfe.even-better-toml
+            techer.open-in-browser
+            tomoki1207.pdf
+            twxs.cmake
+            vadimcn.vscode-lldb
+            vscjava.vscode-gradle
+            vscjava.vscode-java-debug
+            vscjava.vscode-java-dependency
+            vscjava.vscode-java-pack
+            vscjava.vscode-java-test
+            vscjava.vscode-maven
+        ]
+    };
 
     # Config for gtk and qt
     gtk.iconCache.enable = true;
