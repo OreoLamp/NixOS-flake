770a2417b08040966a91c85f2973a3b545e513af
Eero Lampela <eero.lampela@gmail.com>
2023-11-16 21:28:52 +0200

    Reverted one flake.nix change

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
     home-manager,
     ...
   } @ inputs: let
+    inherit (self) outputs;
     
     # Supported systems for your flake packages, shell, etc.
     systems = [
@@ -28,7 +29,7 @@
     
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     
     # Your custom packages
@@ -41,7 +42,7 @@
       
       # Hostname
       desktop-nix = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs self.outputs;};
+        specialArgs = {inherit inputs outputs;};
         modules = [
           # Main config file
           ./system/configuration.nix
